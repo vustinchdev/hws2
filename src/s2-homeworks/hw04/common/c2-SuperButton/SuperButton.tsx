d6e374d854ac4d9298b86b08956065fd92a81774
vustinchdev <vustinchdev@gmail.com>
2023-11-03 20:58:23 +0300

    hw4

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -1,4 +1,4 @@
-import React, {ButtonHTMLAttributes, DetailedHTMLProps} from 'react'
+import React, { ButtonHTMLAttributes, DetailedHTMLProps } from 'react'
 import s from './SuperButton.module.css'
 
 // тип пропсов обычной кнопки, children в котором храниться название кнопки там уже описан
@@ -18,11 +18,13 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
     }
 ) => {
     const finalClassName = s.button
-        // + (disabled
-        //         ? ...
-        //         : xType === 'red'
-        //             ? ...
-        + (className ? ' ' + className : '') // задачка на смешивание классов
+        + ' ' + (disabled
+            ? s.disabled
+            : xType === 'red'
+                ? s.red
+                : xType === 'secondary'
+                    ? s.secondary
+                    : s.default) // задачка на смешивание классов
 
     return (
         <button
