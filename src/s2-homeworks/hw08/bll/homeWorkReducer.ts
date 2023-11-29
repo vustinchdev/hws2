11872f002ee25e2158317ec01fe7ba2341be164c
vustinchdev <vustinchdev@gmail.com>
2023-11-29 13:15:15 +0300

    hw8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,18 +1,20 @@
-import {UserType} from '../HW8'
+import { UserType } from '../HW8'
 
 type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            if (action.payload === 'down') {
+                return [...state].sort((a, b) => b.name > a.name ? 1 : -1)
+            } else {
+                return [...state].sort((a, b) => a.name > b.name ? 1 : -1)
+            }
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(user => user.age >= action.payload) // need to fix
         }
         default:
             return state
