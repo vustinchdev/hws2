ee39edde007ee4d084b5c364a742720b36edda64
vustinchdev <vustinchdev@gmail.com>
2023-12-22 12:15:35 +0300

    hw10

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import {useDispatch, useSelector} from 'react-redux'
-import {AppStoreType} from './bll/store'
-import {loadingAC} from './bll/loadingReducer'
+import { useDispatch, useSelector } from 'react-redux'
+import { AppStoreType } from './bll/store'
+import { loadingAC } from './bll/loadingReducer'
 import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
 import s2 from '../../s1-main/App.module.css'
-import {Loader} from './Loader'
+import { Loader } from './Loader'
 
 /*
 * 1 - в файле loadingReducer.ts дописать типы и логику
@@ -15,11 +15,16 @@ import {Loader} from './Loader'
 
 const HW10 = () => {
     // useSelector, useDispatch // пишет студент
-    const isLoading = false
+    const isLoading = useSelector<AppStoreType, boolean>(state => state.loading.isLoading)
+    const dispatch = useDispatch()
+    // const isLoading = false
 
     const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
         // dispatch
-
+        dispatch(loadingAC(true))
+        setTimeout(() => {
+            dispatch(loadingAC(false))
+        }, 1500)
         // setTimeout
     }
 
@@ -30,7 +35,7 @@ const HW10 = () => {
             <div className={s2.hw}>
                 {isLoading ? (
                     <div id={'hw10-loading'}>
-                        <Loader/>
+                        <Loader />
                     </div>
                 ) : (
                     <SuperButton
--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,11 +1,19 @@
-const initState = {
+const initState: LoadingStateType = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export type LoadingStateType = {
+    isLoading: boolean
+}
+
+export const loadingReducer = (state: LoadingStateType = initState, action: LoadingActionType): LoadingStateType => { // fix any
     switch (action.type) {
         // пишет студент  // need to fix
-
+        case 'CHANGE_LOADING':
+            return {
+                ...state,
+                isLoading: action.isLoading
+            }
         default:
             return state
     }
